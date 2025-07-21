dfd64cd28d499008fc0e05cdbb354bfc858182ac
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2025-07-21 14:34:12 +0900

    [test] 테스트 코드 작성
    - LoginViewModel 테스트코드 작성 진행중

--- presentation/src/main/java/com/llama/presentation/login/LoginScreen.kt
+++ presentation/src/main/java/com/llama/presentation/login/LoginScreen.kt
@@ -21,9 +21,9 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.llama.presentation.main.MainActivity
 import com.llama.presentation.component.LLButton
 import com.llama.presentation.component.LLTextField
+import com.llama.presentation.main.MainActivity
 import com.llama.presentation.theme.ConnectedTheme
 import org.orbitmvi.orbit.compose.collectAsState
 import org.orbitmvi.orbit.compose.collectSideEffect
