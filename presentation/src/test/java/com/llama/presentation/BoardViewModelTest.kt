a31c7efdbc968d185b081acfb4aabf988fd92b7b
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2025-08-03 14:03:22 +0900

    [test] 테스트 코드 작성
    - BoardViewModel 테스트 코드 작성

---
+++ presentation/src/test/java/com/llama/presentation/BoardViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.llama.presentation
+
+import androidx.paging.PagingData
+import com.llama.domain.model.Board
+import com.llama.domain.usecase.main.board.DeleteBoardUseCase
+import com.llama.domain.usecase.main.board.GetBoardUseCase
+import com.llama.presentation.main.board.BoardViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import org.junit.Before
+
+class BoardViewModelTest {
+    private lateinit var viewModel: BoardViewModel
+
+    @Before
+    fun setUp() {
+        val getBoardUseCase = FakeGetBoardUseCase()
+        val deleteBoardUseCase = FakeDeleteBoardUseCase()
+        viewModel = BoardViewModel(
+            getBoardUseCase,
+            deleteBoardUseCase
+        )
+    }
+
+    class FakeGetBoardUseCase : GetBoardUseCase {
+        override suspend fun invoke(): Result<Flow<PagingData<Board>>> = runCatching {
+            emptyFlow()
+        }
+    }
+
+    class FakeDeleteBoardUseCase : DeleteBoardUseCase {
+        override suspend fun invoke(boardId: Long): Result<Long> = runCatching {
+            -1
+        }
+    }
+}
